a4df71268a042759935a8ac88a5deecf2223f55a
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2023-01-02 17:04:26 -0500

    adding body-parser, cookie-parser, compression, helmet and cors modules to express

--- server/express.js
+++ server/express.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
 const app = express()
 /*
 configure express
 */
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
 export default app
