468074bcd536f9fb03869dfb8a47cf53125fd94e
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2023-01-02 21:27:53 -0500

    updating the Home component

---
+++ client/core/Home.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import homeImg from './../assets/images/homeimage.jpg'
+
+const useStyles = makeStyles(theme => ({
+    card: {
+      maxWidth: 600,
+      margin: 'auto',
+      marginTop: theme.spacing(5),
+      marginBottom: theme.spacing(5)
+    },
+    title: {
+      padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+      color: theme.palette.openTitle
+    },
+    media: {
+      minHeight: 400
+    }
+  }))
+
+  export default function Home(){
+    const classes = useStyles()
+      return (
+          <Card className={classes.card}>
+            <Typography variant="h6" className={classes.title}>
+              Home Page
+            </Typography>
+            <CardMedia className={classes.media} image={homeImg} title="Home"/>
+            <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by Anguel Metodiev</Typography>
+            <CardContent>
+              <Typography variant="body1" component="p">
+                Welcome to my home page.
+              </Typography>
+            </CardContent>
+          </Card>
+      )
+  }
