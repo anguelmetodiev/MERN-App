9b9440847e5d1e22e23f7642418121f3e9adae6a
Anguel M. Metodiev <anguelmetodiev@gmail.com>
2023-01-03 09:23:18 -0500

    updating user

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -42,3 +42,20 @@ const create = async (user) => {
       console.log(err)
     }
   }
+
+  const update = async (params, credentials, user) => {
+    try {
+      let response = await fetch('/api/users/' + params.userId, {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + credentials.t
+        },
+        body: JSON.stringify(user)
+      })
+      return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
